8bf9c670eb7830b7862aa3949df18c193eb00851
Dongthach <thachdong270293@gmail.com>
2020-06-30 23:58:16 +0700

    final commit

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-// const path = require("path");
+const path = require("path");
 
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
-// const { cloudinaryConfig } = require("./utils/cloudinaryUpload");
 
 const app = express();
 
 //Connect to mongodb via mongoose
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -25,11 +23,9 @@ mongoose
 
 //Top middleware
 app.use(cors());
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.resolve(__dirname, "public")));
-// app.use(cloudinaryConfig);
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //Routes
 app.use("/api/user", userRoutes);
